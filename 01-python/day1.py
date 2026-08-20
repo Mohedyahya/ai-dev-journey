eae00720024d7032bd5c0b943d1a031e8acf5990
Mohammed dev <m7833yahya@gmail.com>
2026-08-20 23:33:43 +0300

    first day

---
+++ 01-python/day1.py
@@ -0,0 +1,17 @@
+# Bismillah - Day 1
+print("=" * 40)
+print("Hello World!")
+print("=" * 40)
+
+name = "Mohammed"
+country = "yemen"
+dream = "AI developer"
+
+print("Name" , name)
+print("Country" , country)
+print("Goal" , dream)
+print("=" * 40)
+
+print("2026 +2 =",2026 + 2)
+print("By 2028 I well be an ", dream)
+print("=" * 40)
